30ee558c7e2c22bafcaa6fe6151ea583c4218a0d
Ylenia <yleniamighela@gmail.com>
2025-03-10 16:14:09 +0100

    added reviews SHOW

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -28,18 +28,49 @@ function show(req, res) {
     // salviamo in una costante il valore dell'id dai params
     const { id } = req.params;
 
-    // mi salvo in una costante la query
+
+    // mi salvo in una costante la query dei dettagli del film
     const detailMovie = 'SELECT * FROM movies WHERE movies.id = ?';
 
-    // eseguo la query
+
+    // mi salvo in una costante la query per delle recensioni
+    const review = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+
+
+    // eseguo la query per i film
     connection.query(detailMovie, [id], (err, movieResult) => {
 
         if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (movieResult.length === 0) return res.status(404).json({ error: 'Movie not found' });
 
-        if (movieResult.length === 0) return res.status(404).json({ error: 'Post not found' });
 
-        res.json(movieResult[0]);
+
+        const movie = movieResult[0];
+
+        // se la prima query va a buoon fine allora eseguo la query per le recensioni
+        connection.query(review, [id], (err, reviewResult) => {
+
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+
+            // aggiungo la prorpietà review ai film
+            movie.reviews = reviewResult;
+
+            // ritorno l'oggetto aggiornato
+            res.json(movie);
+
+        });
+
     });
+
+
+
+
+
+
+
+
 };
 
 
