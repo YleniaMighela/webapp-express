c65e1d4f5b5695384fbf0439aad486fb8b359c74
Ylenia <yleniamighela@gmail.com>
2025-03-14 15:22:21 +0100

    added install multer

---
+++ middlewares/multer.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+
+// file upload middleware
+const storage = multer.diskStorage({
+    destination: './public/img/movies_cover/',
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueName);
+    },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
